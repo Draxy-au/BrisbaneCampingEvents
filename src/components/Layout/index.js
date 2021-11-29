a344faa594b8a476acbf90ebcfed0499231d6e2a
William Hamilton <draxy80@gmail.com>
2021-11-30 08:56:05 +1000

    Updated JsonConfig with paths relative to baseURL. Added 404 Page.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import Header from 'components/Header';
-import styles from 'styles/Layout.module.css';
-import Footer from 'components/Footer';
+import Header from '@/components/Header';
+import styles from '@/styles/Layout.module.css';
+import Footer from '@/components/Footer';
 
 export default function Layout({title, keywords, description, children}) {
   return (
