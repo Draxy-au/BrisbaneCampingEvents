5954c9a90766e799b1f67a11a45794afc3093f54
William Hamilton <draxy80@gmail.com>
2021-11-30 14:32:13 +1000

    Added in Event Data and presentation

---
+++ src/components/Showcase/index.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome Campers!</h1>
+      <h2>Find upcoming Camping events</h2>
+    </div>
+  )
+}
